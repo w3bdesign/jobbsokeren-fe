c8b907e3a97ae6d5779ac8f2f165b5b48518561f
Mats Gundersen <matsgundersen@hotmail.com>
2023-05-08 12:36:21 +0200

    added a new component for article card

---
+++ src/pages/articlesPage/components/ArticleCard.tsx
@@ -0,0 +1,49 @@
+import React from 'react';
+
+interface Article {
+    title: string;
+    createdAt: string;
+    description: string;
+    image: string;
+}
+
+interface Props {
+    article: Article;
+  }
+
+const ArticleCard: React.FC<Props> = ({ article }) => {
+    const {title, description, createdAt, image} = article;
+    return (
+        <div className="w-full px-4 md:w-1/2 lg:w-1/3 cursor-pointer">
+            <div className="mx-auto mb-10 max-w-[370px]">
+            <div className="mb-8 overflow-hidden rounded">
+                <img
+                src="https://cdn.tailgrids.com/2.0/image/application/images/blogs/blog-01/image-03.jpg"
+                alt="image"
+                className="w-full"
+                />
+            </div>
+            <div>
+                <span
+                className="border bg-indigo-600 border-indigo-600 mb-5 inline-block rounded py-1 px-4 text-center text-xs font-semibold leading-loose text-white"
+                >
+               {createdAt}
+                </span>
+                <h3>
+                <a
+                    href="javascript:void(0)"
+                    className="text-dark hover:text-primary mb-4 inline-block text-xl font-semibold sm:text-2xl lg:text-xl xl:text-2xl"
+                >
+                    {title}
+                </a>
+                </h3>
+                <p className="text-body-color text-base">
+                {description}
+                </p>
+            </div>
+            </div>
+        </div>
+    )
+}
+
+export default ArticleCard
--- src/pages/articlesPage/index.tsx
+++ src/pages/articlesPage/index.tsx
@@ -1,121 +1,46 @@
+import ArticleCard from './components/ArticleCard';
+import React from 'react';
 
 
-const Articles = () => {
+const article = {
+    title: 'Slik skriver du en god søknad',
+    createdAt: '2021-09-01',
+    description: 'Søknaden er det første arbeidsgiveren ser av deg. Den skal vekke interesse og gi et godt førsteinntrykk. Her er noen tips til hvordan du kan skrive en god søknad.',
+    image: 'https://cdn.tailgrids.com/2.0/image/application/images/blogs/blog-01/image-03.jpg'
+}
+
+const Articles: React.FC = () => {
     return (
-        <>
-            <section className="pt-20 pb-10 lg:pt-[120px] lg:pb-20">
-            <div className="container mx-auto">
-                <div className="-mx-4 flex flex-wrap justify-center">
-                <div className="w-full px-4">
-                    <div className="mx-auto mb-[60px] max-w-[510px] text-center lg:mb-20">
-                    <span className="text-primary mb-2 block text-lg font-semibold">
-                        Our Blogs
-                    </span>
-                    <h2
-                        className="text-dark mb-4 text-3xl font-bold sm:text-4xl md:text-[40px]"
-                    >
-                        Our Recent News
-                    </h2>
-                    <p className="text-body-color text-base">
-                        There are many variations of passages of Lorem Ipsum available but
-                        the majority have suffered alteration in some form.
-                    </p>
-                    </div>
-                </div>
-                </div>
-                <div className="-mx-4 flex flex-wrap">
-                <div className="w-full px-4 md:w-1/2 lg:w-1/3">
-                    <div className="mx-auto mb-10 max-w-[370px]">
-                    <div className="mb-8 overflow-hidden rounded">
-                        <img
-                        src="https://cdn.tailgrids.com/2.0/image/application/images/blogs/blog-01/image-01.jpg"
-                        alt="image"
-                        className="w-full"
-                        />
-                    </div>
-                    <div>
-                        <span
-                        className="bg-primary mb-5 inline-block rounded py-1 px-4 text-center text-xs font-semibold leading-loose text-white"
-                        >
-                        Dec 22, 2023
+        <>  
+            <section className="pt-20 pb-10 lg:pt-[120px] lg:pb-20 bg-zinc-100">
+                <div className="bg-zinc-100 h-[70px]"></div>
+                <div className="container mx-auto">
+                    <div className="-mx-4 flex flex-wrap justify-center">
+                    <div className="w-full px-4">
+                        <div className="mx-auto mb-[60px] max-w-[510px] text-center lg:mb-20">
+                        <span className="text-indigo-600 mb-2 block text-lg font-semibold">
+                            Artikler
                         </span>
-                        <h3>
-                        <a
-                            href="javascript:void(0)"
-                            className="text-dark hover:text-primary mb-4 inline-block text-xl font-semibold sm:text-2xl lg:text-xl xl:text-2xl"
+                        <h2
+                            className="text-dark mb-4 text-3xl font-bold sm:text-5xl"
                         >
-                            Meet AutoManage, the best AI management tools
-                        </a>
-                        </h3>
-                        <p className="text-body-color text-base">
-                        Lorem Ipsum is simply dummy text of the printing and typesetting
-                        industry.
+                            Søknader og CV tips
+                        </h2>
+                        <p className="text-2xl py-2 text-gray-500">
+                        Mestre kunsten å skrive overbevisende søknader og CV-er som utmerker seg i konkurransen
                         </p>
+                        </div>
                     </div>
                     </div>
-                </div>
-                <div class="w-full px-4 md:w-1/2 lg:w-1/3">
-                    <div className="mx-auto mb-10 max-w-[370px]">
-                    <div className="mb-8 overflow-hidden rounded">
-                        <img
-                        src="https://cdn.tailgrids.com/2.0/image/application/images/blogs/blog-01/image-02.jpg"
-                        alt="image"
-                        className="w-full"
-                        />
-                    </div>
-                    <div>
-                        <span
-                        className="bg-primary mb-5 inline-block rounded py-1 px-4 text-center text-xs font-semibold leading-loose text-white"
-                        >
-                        Mar 15, 2023
-                        </span>
-                        <h3>
-                        <a
-                            href="javascript:void(0)"
-                            className="text-dark hover:text-primary mb-4 inline-block text-xl font-semibold sm:text-2xl lg:text-xl xl:text-2xl"
-                        >
-                            How to earn more money as a wellness coach
-                        </a>
-                        </h3>
-                        <p className="text-body-color text-base">
-                        Lorem Ipsum is simply dummy text of the printing and typesetting
-                        industry.
-                        </p>
-                    </div>
+                    <div className="-mx-4 flex flex-wrap gap-y-6">
+                        <ArticleCard article={article} />
+                        <ArticleCard article={article} />
+                        <ArticleCard article={article} />
+                        <ArticleCard article={article} />
+                        <ArticleCard article={article} />
+                        <ArticleCard article={article} />
                     </div>
                 </div>
-                <div className="w-full px-4 md:w-1/2 lg:w-1/3">
-                    <div className="mx-auto mb-10 max-w-[370px]">
-                    <div className="mb-8 overflow-hidden rounded">
-                        <img
-                        src="https://cdn.tailgrids.com/2.0/image/application/images/blogs/blog-01/image-03.jpg"
-                        alt="image"
-                        className="w-full"
-                        />
-                    </div>
-                    <div>
-                        <span
-                        className="bg-primary mb-5 inline-block rounded py-1 px-4 text-center text-xs font-semibold leading-loose text-white"
-                        >
-                        Jan 05, 2023
-                        </span>
-                        <h3>
-                        <a
-                            href="javascript:void(0)"
-                            className="text-dark hover:text-primary mb-4 inline-block text-xl font-semibold sm:text-2xl lg:text-xl xl:text-2xl"
-                        >
-                            The no-fuss guide to upselling and cross selling
-                        </a>
-                        </h3>
-                        <p className="text-body-color text-base">
-                        Lorem Ipsum is simply dummy text of the printing and typesetting
-                        industry.
-                        </p>
-                    </div>
-                    </div>
-                </div>
-                </div>
-            </div>
             </section>  
         </>
     )
