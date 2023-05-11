bac665bf56185a26be933059eee91885e53ad98b
Mats Gundersen <matsgundersen@hotmail.com>
2023-05-11 14:25:30 +0200

    fix

--- src/pages/articlePage/index.tsx
+++ src/pages/articlePage/index.tsx
@@ -33,7 +33,7 @@ const ArticlePage : React.FC = () => {
         {error && <ErrorDisplayer title="ups!" errorMessage="Noe gikk galt når vi prøvde å hente artikler. Prøv igjen senere." errorCode={500} /> }
         {!articles && !error && <LoadingDisplayer />}
         {articles && 
-        <div className="relative isolate overflow-hidden bg-zinc-100 mt-5 px-6 py-24 sm:py-32 lg:overflow-visible lg:px-0">
+        <div className="relative isolate bg-zinc-100 mt-5 px-6 py-24 sm:py-32 overflow-visible lg:px-0">
           <div className="mx-auto grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 lg:mx-0 lg:max-w-none lg:grid-cols-2 lg:items-start lg:gap-y-10">
             <div className="lg:col-span-2 lg:col-start-1 lg:row-start-1 lg:mx-auto lg:grid lg:w-full lg:max-w-7xl lg:grid-cols-2 lg:gap-x-8 lg:px-8">
               <div className="lg:pr-4">
