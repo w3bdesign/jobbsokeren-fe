eff5871f6df3376a2e5cfee561c732b828b7f148
Mats Gundersen <matsgundersen@hotmail.com>
2023-05-11 14:04:07 +0200

    small loading component chagne

--- src/components/LoadingDisplayer.tsx
+++ src/components/LoadingDisplayer.tsx
@@ -1,8 +1,22 @@
 import { ThreeCircles } from "react-loader-spinner";
-import React from 'react';
-
+import React, { useState, useEffect } from 'react';
 
 const LoadingDisplayer : React.FC = () => {
+    const [isVisible, setIsVisible] = useState(false);
+
+    useEffect(() => {
+        const timer = setTimeout(() => {
+            setIsVisible(true);
+        }, 300); // Change this value to adjust the delay
+
+        // Cleanup function to clear the timeout if the component is unmounted
+        return () => clearTimeout(timer);
+    }, []);
+
+    if (!isVisible) {
+        return null;
+    }
+
     return (
       <>
         <div className='h-full absolute w-full z-40 grid'>
@@ -11,13 +25,11 @@ const LoadingDisplayer : React.FC = () => {
                     width={100}
                     color="#4f46e5"                
                     wrapperClass="m-auto "
-                    visible={true}
+                    visible={isVisible}
                     ariaLabel="three-circles-rotating" />
             </div>
       </>
     )
-  }
+}
 
-export default LoadingDisplayer
-
-  
+export default LoadingDisplayer;
