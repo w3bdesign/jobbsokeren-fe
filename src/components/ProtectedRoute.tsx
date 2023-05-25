26503ad3ce47d346b90932bb587d237aa3cd329f
Mats Gundersen <matsgundersen@hotmail.com>
2023-05-25 10:43:51 +0200

    added files

---
+++ src/components/ProtectedRoute.tsx
@@ -0,0 +1,17 @@
+import { Navigate } from 'react-router-dom';
+import { FirebaseUser } from '@/models/firebaseUserModel';
+
+interface ProtectedRouteProps {
+    children: React.ReactNode; 
+    user: FirebaseUser | null; // add user prop
+}
+
+const ProtectedRoute: React.FC<ProtectedRouteProps> = ({ children, user }) => {
+    if (!user) {
+      return <Navigate to="/" />; // replace '/' with your login page path if available
+    }
+  
+    return <>{children}</>;
+};
+
+export default ProtectedRoute;
---
+++ src/pages/profilePage/index.tsx
@@ -0,0 +1,10 @@
+
+const profilePage = () => {
+    return (
+        <div>
+            <h1>Hello</h1>
+        </div>
+    )
+}
+
+export default profilePage
