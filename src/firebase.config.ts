5ef717109daecf04603a84d476a31593f465b2ed
Mats Gundersen <matsgundersen@hotmail.com>
2023-05-25 09:52:56 +0200

    changed firebase values

--- src/firebase.config.ts
+++ src/firebase.config.ts
@@ -5,6 +5,10 @@ import { getAuth} from "firebase/auth";
 const apiKey = import.meta.env.VITE_FIREBASE_API_KEY;
 const appId = import.meta.env.VITE_FIREBASE_APP_ID;
 const messagingSenderId = import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID;
+const authDomain = import.meta.env.VITE_FIREBASE_AUTH_DOMAIN;
+const projectId = import.meta.env.VITE_FIREBASE_PROJECT_ID;
+const storageBucket = import.meta.env.VITE_FIREBASE_STORAGE_BUCKET;
+
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -12,9 +16,9 @@ const messagingSenderId = import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID;
 // Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: apiKey,
-  authDomain: "jobbsoknader-dev.firebaseapp.com",
-  projectId: "jobbsoknader-dev",
-  storageBucket: "jobbsoknader-dev.appspot.com",
+  authDomain:  authDomain,
+  projectId: projectId,
+  storageBucket: storageBucket,
   messagingSenderId: messagingSenderId,
   appId: appId,
 };
