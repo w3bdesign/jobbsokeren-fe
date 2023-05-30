a371897588679f0c5e743880fa56583a09997b1d
Mats Gundersen <matsgundersen@hotmail.com>
2023-05-30 15:53:04 +0200

    added file for fileInput

---
+++ src/components/FileInput.tsx
@@ -0,0 +1,64 @@
+import React, { useState } from 'react';
+import { DocumentTextIcon } from '@heroicons/react/24/outline';
+
+interface FileInputProps {
+    name?: string | undefined | null;
+    image?: string | undefined | null;
+}
+
+const FileInput: React.FC<FileInputProps> = (props: FileInputProps) => {
+    const [file, setFile] = useState<File | null>(null);
+
+    const handleDragOver = (event: React.DragEvent<HTMLLabelElement>) => {
+        event.preventDefault();
+    };
+
+    const handleDrop = (event: React.DragEvent<HTMLLabelElement>) => {
+        event.preventDefault();
+        if (event.dataTransfer.items) {
+            const fileItem = event.dataTransfer.items[0];
+            if (fileItem.kind === 'file') {
+                const droppedFile = fileItem.getAsFile();
+                if (droppedFile && droppedFile.type === 'application/pdf') {
+                    setFile(droppedFile);
+                } else {
+                    alert('Beklager! Vi støtter for øyeblikket bare PDF-filer.');
+                }
+            }
+        }
+    };
+
+    return (
+        <div className="flex items-center justify-center w-full">
+            <label htmlFor="dropzone-file" 
+                className="flex flex-col items-center justify-center w-full h-64 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50 hover:bg-gray-100"
+                onDragOver={handleDragOver}
+                onDrop={handleDrop}
+            >
+                {file ? (
+                   <div className="flex items-center space-x-2">
+                        <DocumentTextIcon className="w-6 h-6 text-gray-500" />
+                   <p className="text-sm text-gray-500">Fil: {file.name}</p>
+               </div>
+                ) : (
+                    <div className="flex flex-col items-center justify-center pt-5 pb-6">
+                        <svg aria-hidden="true" className="w-10 h-10 mb-3 text-gray-400" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M7 16a4 4 0 01-.88-7.903A5 5 0 1115.9 6L16 6a5 5 0 011 9.9M15 13l-3-3m0 0l-3 3m3-3v12"></path>
+                        </svg>
+                        <p className="mb-2 text-sm text-gray-500"><span className="font-semibold">Klikk for å laste opp CV</span> Eller dra inn filen</p>
+                        <p className="text-xs text-gray-500">PDF</p>
+                    </div>
+                )}
+                <input id="dropzone-file" type="file" className="hidden" onChange={event => {
+                    if (event.target.files && event.target.files[0].type === 'application/pdf') {
+                        setFile(event.target.files[0]);
+                    } else {
+                        alert('Please upload a PDF file.');
+                    }
+                }} />
+            </label>
+        </div>
+    )
+}
+
+export default FileInput;
--- src/pages/profilePage/components/ApplicantInfoForm.tsx
+++ src/pages/profilePage/components/ApplicantInfoForm.tsx
@@ -1,84 +1,14 @@
 import React, {useState} from "react";
-import TextInput from "@/components/TextInput";
-import { EditorFormModel } from "@/models/editorFormModel";
-import AvatarText from "@/components/AvatarText";
 import { useSelector } from "react-redux";
 import { RootState } from "@/store/store";
 import SubmitButton from "@/components/SubmitButton";
+import FileInput from "@/components/FileInput";
 
-const textInputConfigs = [
-    {
-      id: "applicant_name",
-      name: "applicant_name",
-      autoComplete: "on",
-      type: "text",
-      placeholder: "Ola Norman",
-      required: true,
-      label: "Navn",
-      width: 48,
-    },
-    {
-      id: "applicant_email",
-      name: "applicant_email",
-      autoComplete: "on",
-      type: "email",
-      placeholder: "olanorman@gmail.com",
-      required: true,
-      label: "E-post",
-      width: 48,
-    },
-    {
-      id: "applicant_address",
-      name: "applicant_address",
-      autoComplete: "on",
-      type: "text",
-      placeholder: "Slottsparken 1",
-      required: true,
-      label: "Gateadresse",
-      width: 48,
-    },
-    {
-      id: "applicant_zip_code",
-      name: "applicant_zip_code",
-      autoComplete: "on",
-      type: "text",
-      pattern: "[0-9]*",
-      minLength: 4,
-      maxLength: 4,
-      placeholder: "5071",
-      required: true,
-      label: "Postnummer",
-      width: 48,
-    },
-    {
-      id: "applicant_city",
-      name: "applicant_city",
-      autoComplete: "on",
-      type: "text",
-      placeholder: "Bergen",
-      required: true,
-      label: "By",
-      width: 48,
-    },
-  ];
 
-  const emptyFormValues: EditorFormModel = {
-    applicant_name: '',
-    applicant_email: '',
-    applicant_address: '',
-    applicant_zip_code:'',
-    applicant_city: '',
-    applicant_job_advertisement_url: ''
-}
 
 const ApplicantInfoForm: React.FC = () => {
     const user = useSelector((state: RootState) => state.auth.user);
-    const [formValues, setFormValues] = useState<EditorFormModel>(emptyFormValues);
     
-    const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-        const { name, value } = event.target;
-        setFormValues({ ...formValues, [name]: value });
-    };
     return (
         <>
             <div className="w-full">
@@ -90,24 +20,11 @@ const ApplicantInfoForm: React.FC = () => {
                                     <p className="mt-1 text-sm leading-6 text-gray-600">Vennligst last opp din CV. Vi vil hente informasjon fra CV-en når vi lager jobbsøknad for å skreddersy din søknad så mye som mulig ut i fra dine erfaringer, kompetanse og personlige egenskaper.</p>
                                 </div>
                             <div className="flex flex-wrap justify-between">
-                            {textInputConfigs.map((textInputConfig, index) => (
-                                <TextInput
-                                    key={index}
-                                    id={textInputConfig.id}
-                                    name={textInputConfig.name}
-                                    value={formValues[textInputConfig.id as keyof EditorFormModel]}
-                                    autoComplete={textInputConfig.autoComplete}
-                                    type={textInputConfig.type}
-                                    placeholder={textInputConfig.placeholder}
-                                    onChange={handleInputChange}
-                                    required={textInputConfig.required}
-                                    label={textInputConfig.label}
-                                    width={textInputConfig.width}/>
-                                ))}
+                                <FileInput />
                             </div>
                         </div>
                         <div className="sm:w-1/3 ml-auto">
-                            <SubmitButton isLoading={false} buttonText={"Lagre endringer"}/>
+                            <SubmitButton isLoading={false} buttonText={"Last opp CV"}/>
                         </div>
                     </div>
                 </form>
