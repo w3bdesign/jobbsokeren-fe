83fbfcf69c12fcb84f2c3078b865f7fdceac6ff5
Mats Gundersen <matsgundersen@hotmail.com>
2023-05-30 10:14:30 +0200

    fixed some issues with sidebar

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -4,8 +4,8 @@ import { useSelector } from 'react-redux';
 import Login from "./Login";
 import Logout from "./logout";
 import { RootState } from '@/store/store';
+import { useRef, useEffect } from "react";
 
-// toggle this class -translate-x-full to show/hide the sidebar
 
 interface SideBarProps {
     showSideBar: boolean;
@@ -15,9 +15,31 @@ interface SideBarProps {
 const SideBar : React.FC<SideBarProps> =({showSideBar, handleSideBar}) => {
 
     const user = useSelector((state : RootState) => state.auth.user);
+    const sidebarRef = useRef<HTMLDivElement | null>(null);
+
+    // function to handle click outside the sidebar
+    // close the sidebar if clicked outside
+    const handleClickOutside = (event: MouseEvent) => {
+        if (sidebarRef.current && event.target instanceof Node && !sidebarRef.current.contains(event.target)) {
+            handleSideBar();
+        }
+    };
+
+    useEffect(() => {
+        if (showSideBar) {
+            window.addEventListener('mousedown', handleClickOutside);
+        } else {
+            window.removeEventListener('mousedown', handleClickOutside);
+        }
+        return () => {
+            window.removeEventListener('mousedown', handleClickOutside);
+        };
+    }, [showSideBar]);
+
+
 
     return (
-        <div id="drawer-navigation"  className={`fixed top-20 right-0 z-40 w-72 h-screen p-4 overflow-y-auto transition-transform duration-300 transform bg-zinc-50 drop-shadow-md ${showSideBar ? 'translate-x-0' : 'translate-x-full'}`}  aria-labelledby="drawer-navigation-label">
+        <div id="drawer-navigation" ref={sidebarRef} className={`fixed top-20 right-0 z-40 w-72 h-screen p-4 overflow-y-auto transition-transform duration-300 transform bg-zinc-50 drop-shadow-md ${showSideBar ? 'translate-x-0' : 'translate-x-full'}`}  aria-labelledby="drawer-navigation-label">
              <h5 id="drawer-navigation-label" className="text-base font-semibold text-gray-500 uppercase">{user?.displayName}</h5> 
             <button onClick={handleSideBar} type="button" className="absolute top-3 right-6 text-gray-400 bg-transparent border-none hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center">
                 <XMarkIcon className="w-6 h-6 text-gray-600" aria-hidden="true" />
@@ -26,7 +48,7 @@ const SideBar : React.FC<SideBarProps> =({showSideBar, handleSideBar}) => {
             <div className="py-8 overflow-y-auto">
                 <ul className="space-y-2 font-medium text-gray-900">
                 <li>
-                    <RouterLink to="/profil" className={`inline-flex items-center p-2 hover:text-indigo-600 ${user ? '' : 'opacity-50 cursor-not-allowed'}`}>
+                    <RouterLink onClick={handleSideBar} to="/profil" className={`inline-flex items-center p-2 hover:text-indigo-600 ${user ? '' : 'opacity-50 cursor-not-allowed'}`}>
                     <div className="flex items-center">
                         <UserIcon className="w-6 h-6 mr-3 text-indigo-600" aria-hidden="true" />
                             Min profil
@@ -34,7 +56,7 @@ const SideBar : React.FC<SideBarProps> =({showSideBar, handleSideBar}) => {
                     </RouterLink>
                 </li>
                 <li>
-                    <RouterLink to="#" className={`inline-flex items-center p-2 hover:text-indigo-600 ${user ? '' : 'opacity-50 cursor-not-allowed'}`}>
+                    <RouterLink onClick={handleSideBar}  to="#" className={`inline-flex items-center p-2 hover:text-indigo-600 ${user ? '' : 'opacity-50 cursor-not-allowed'}`}>
                     <div className="flex items-center">
                         <NewspaperIcon className="w-6 h-6 mr-3 text-indigo-600" aria-hidden="true" />
                             Mine søknader
