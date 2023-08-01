a4a825ed3a804e8253b672b0dab6b198af612554
Mats Gundersen <matsgundersen@hotmail.com>
2023-08-01 10:36:28 +0200

    added delete of summary when file is deleted

--- src/hooks/useDeleteFirebaseUserFile.ts
+++ src/hooks/useDeleteFirebaseUserFile.ts
@@ -1,7 +1,8 @@
+import { doc, deleteDoc } from 'firebase/firestore';
 import { ref, deleteObject } from 'firebase/storage';
 import { useState } from 'react';
 
-import { storage } from '@/firebase.config';
+import { storage, db } from '@/firebase.config';
 
 interface UseDeleteFirebaseUserFileProps {
     deleteFile: (user: { uid: string } | null | undefined) => Promise<boolean>,
@@ -13,21 +14,34 @@ const useDeleteFirebaseUserFile = (): UseDeleteFirebaseUserFileProps => {
   const [error, setError] = useState<Error | null>(null);
   const [isLoading, setIsLoading] = useState(false);
 
-  const deleteFile = (user: { uid: string } | null | undefined): Promise<boolean> => {
-    return new Promise<boolean>((resolve, reject) => {
-      setIsLoading(true);
-      const fileRef = ref(storage, `users_cv_storage/${user?.uid}/${user?.uid}.pdf`);
+  const deleteFile = async (user: { uid: string } | null | undefined): Promise<boolean> => {
+    setIsLoading(true);
+    
+    if (!user?.uid) {
+      setError(new Error('User ID is undefined'));
+      setIsLoading(false);
+      return false;
+    }
   
-      deleteObject(fileRef).then(() => {
-          setIsLoading(false);
-          resolve(true);
-      }).catch((error) => {
-          setIsLoading(false);
-          setError(error);
-          reject(error);
-      });
-    });
+    const fileRef = ref(storage, `users_cv_storage/${user.uid}/${user.uid}.pdf`);
+  
+    try {
+      // Delete the file
+      await deleteObject(fileRef);
+  
+      // Delete the summary from Firestore
+      const userRef = doc(db, 'cvSummaries', user.uid);
+      await deleteDoc(userRef);
+  
+      setIsLoading(false);
+      return true;
+    } catch (error) {
+      setIsLoading(false);
+      setError(error instanceof Error ? error : new Error(String(error)));
+      return false;
+    }
   };
+  
 
   return { deleteFile, error, isLoading };
 };
