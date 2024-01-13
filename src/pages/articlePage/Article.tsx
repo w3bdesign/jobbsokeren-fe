5d6abecae8d17aea68a8be4b43fe74d72e4f4f5b
Mats Gundersen <matsgundersen@hotmail.com>
2024-01-13 16:48:02 +0100

    change

--- src/pages/articlePage/Article.tsx
+++ src/pages/articlePage/Article.tsx
@@ -46,7 +46,7 @@ const ArticleContent : React.FC = () => {
            </div>
            <div className="-ml-12 -mt-12 p-12 lg:sticky lg:top-4 lg:col-start-2 lg:row-span-2 lg:row-start-1 lg:overflow-hidden">
              <img
-               className="w-[48rem] max-w-none rounded-xl bg-gray-900 shadow-xl ring-1 ring-gray-400/10 sm:w-[57rem]"
+               className=" w-full md:w-[48rem] max-w-none rounded-xl bg-gray-900 shadow-xl ring-1 ring-gray-400/10 sm:w-[57rem]"
                src={article?.image.asset.url}
                alt={article?.title}
              />
